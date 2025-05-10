c6c93f182a273eb4400c76ce33c857e9f5672926
Ahmed Elnemr <ALNME5716@gmail.com>
2025-05-10 20:49:10 +0300

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,28 @@
+import { Outlet, Route, Routes } from 'react-router'
 import './App.css'
+import Home from './pages/Home'
+import About from './pages/About'
+import Contact from './pages/Contact'
+import AllDoctors from './pages/AllDoctors'
+import Login from './pages/Login'
+import Doctor from './pages/Doctor'
 
 function App() {
   
 
   return (
     <div className="App">
-      <h1>welcome</h1>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/about' element={<About/>}/>
+        <Route path='/contact' element={<Contact/>}/>
+        <Route path='/allDocutors' element={<Outlet/>}>
+          <Route path='/allDocutors' element={<AllDoctors/>}/>
+          <Route path=':spcialist' element={<AllDoctors/>}/>
+        </Route>
+        <Route path='/login' element={<Login/>}/>
+        <Route path='/doctor/:id' element={<Doctor/>}/>
+      </Routes>
     </div>
   )
 }
