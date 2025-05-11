1e674fbe8666452faac84362e27f6edba2326b57
dev-ramadan <ganna123adam@gmail.com>
2025-05-11 21:02:41 +0300

    handle home error

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,9 @@
-<<<<<<< HEAD
 
-=======
-import React from 'react'
+
 import Hero from '../components/hero/Hero'
 import HomeTitle from '../components/homeTitle/HomeTitle'
 import Specialisty from '../components/specialistySection/Specialisty'
->>>>>>> 71c7603ab5c4263983a807d5ece9bb3a115fbb43
+
 
 const Home = () => {
     return (
